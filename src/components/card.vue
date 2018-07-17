9459266fdb37e1d9edbb6f1e151c2a33eb1049ad
David Jung <dav@u.northwestern.edu>
2018-07-17 17:24:36 -0500

    x

--- src/components/card.vue
+++ src/components/card.vue
@@ -2,25 +2,24 @@
   
   
     
-  
+  <v-card>
     <article class="people-small" style="height:300px;">
-      <div class="people-wrap">
-          <div class="people-image" style="float:left; width:20%; height:100vh; overflow:hidden ">
+      <div class="people-wrap" style="position: absolute; float:left;">
+          <div class="people-image" style="float:left; width:20%; overflow:hidden ">
               <img src="../assets/170x170.gif" alt="" />           
           </div>
-          <div class="people-content" style="float:left; width:80%; height:100vh; overflow:hidden; background:red;">
+          <div class="people-content" style="float:left; width:80%; height:170px; overflow:hidden; background:red;">
               <h3>{{ student.name.first }} {{ student.name.last }}</h3>
               <p class="title">{{ student.schoolStr }}</p>
               <p>Phone number: 123.456.7890<br />
               <a href="#">email@northwestern.edu</a></p>
           </div>
       </div>
-      
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel hendrerit massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut dapibus enim orci. Nullam auctor ex sit amet diam elementum sagittis. Aliquam sollicitudin eros vel leo volutpat condimentum. Aliquam pulvinar turpis a consectetur consequat. Aliquam sed dolor elementum, eleifend metus sit amet.</p>    
-
-
+      <div style="position: absolute; float: left; clear: left;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel hendrerit massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut dapibus enim orci. Nullam auctor ex sit amet diam elementum sagittis. Aliquam sollicitudin eros vel leo volutpat condimentum. Aliquam pulvinar turpis a consectetur consequat. Aliquam sed dolor elementum, eleifend metus sit amet.</p>    
+      </div>
     </article>  
-
+  </v-card>
   
 
 </template>
